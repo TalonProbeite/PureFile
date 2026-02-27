5d41f37817a542a694b116ebfa129587b505c942
artem ponomarcev <siionunstoppable@gmail.com>
2026-02-27 22:38:15 +0300

    refactore: Change the file name from metadata.py to metadata_read.py , added logging of file types from users.

--- app/routes/metadata_routes.py
+++ app/routes/metadata_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter , UploadFile , HTTPException
 from loguru import logger
 
-from ..services.metadata import get_ext , get_metadata_docx , get_metadata_img , get_metadata_pdf , MetadataReadError
+from ..services.metadata_read import get_ext , get_metadata_docx , get_metadata_img , get_metadata_pdf , MetadataReadError
 
 
 router = APIRouter( tags=["read_metadata"])
@@ -24,6 +24,7 @@ async def get_metadata(upload_file: UploadFile)->dict:
                         result = get_metadata_docx(upload_file.file)
                     case _:
                         raise HTTPException(status_code=422, detail="Incorrect data format. Only pdf, docx, png, jpg , jpeg are supported.")
+            logger.info(f"Processed file type: {ext.upper()}")
             return result
         except MetadataReadError as e:
             original_err_name = type(e.__cause__).__name__ if e.__cause__ else "UnknownError"
